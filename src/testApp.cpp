54abcb7b866ed31b267dad42816d8de127b4edef
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2012-11-06 08:25:48 -0500

    closing things

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -6,102 +6,58 @@ void testApp::setup(){
     ofEnableAlphaBlending();
     ofBackground(0, 0, 0);
     
-    textWidth = 800;
-    textHeight = 600;
-    
-    
     //  Load two MapaMoks
     //
-    mapamoko01.loadSettings("viewport01.xml");
-    mapamoko01.loadMesh("cubos_demo.dae", textWidth, textHeight);
-    mapamoko01.drawMode = DRAW_FACES;
-    
-    mapamoko02.loadSettings("viewport02.xml");
-    mapamoko02.loadMesh("cubos_demo.dae", textWidth, textHeight);
+    mapamoko.loadSettings("viewport.xml");
+    mapamoko.init(0,0,ofGetScreenWidth(), ofGetScreenHeight());
+    mapamoko.loadMesh("cubos_demo.dae", 800, 600);
+    mapamoko.drawMode = DRAW_FACES;
     
     if (shader.load("shader")){
         //  One of the MapaMoks will have a cracy shader
         //
-        mapamoko02.linkShader(&shader);
+        mapamoko.linkShader(&shader);
     }
     
     //  Setup the canvas
     //
-    canvas.allocate(textWidth, textHeight);
+    canvas.allocate(ofGetScreenWidth(), ofGetScreenHeight());
     canvas.begin();
     ofClear(0);
     canvas.end();
-    
-    //  Init WebCam texture
-    //
-    video.initGrabber(640,480);
-    videoViewPort.loadSettings("video.xml");
-    
-    bCanvasMode = false;
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-    video.update();
+        
+    mapamoko.update();
     
-    //  Render the canvas
-    //
-    canvas.begin();
+    canvas.begin(false);
     ofClear(0);
-    video.draw(videoViewPort);
+    mapamoko.draw( );
     canvas.end();
-    
-    if (!bCanvasMode){
-        mapamoko01.update();
-        mapamoko02.update();
-    }
     
     ofSetWindowTitle(ofToString(ofGetFrameRate()));
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
+    ofBackgroundGradient(ofColor::gray, ofColor::black);
     
-    if (bCanvasMode){
-        ofSetColor(255);
-        canvas.draw(0, 0);
-        ofDrawBitmapString("CANVAS MODE ( press 'e' to drag )", 15,15);
-        
-    } else {
-        
-        //
-        //  Para editar el viewPort tiene q apretar 'E' as’ no se solapa con el resto
-        //
-        
-        mapamoko01.draw( &canvas.getTextureReference() );
-        mapamoko02.draw( );
-    }
+    ofSetColor(255);
+    canvas.draw(0, 0);
+    
+    //mapamoko.draw( );
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
     if(key == 'f'){
-        
-        //  Toggle Fullscreen
-        //
         ofToggleFullscreen();
-    } else if(key == 'c'){
-        
-        //  Canvas Mode to edit what is proyected over the surface
-        //
-        bCanvasMode = !bCanvasMode;
     } else if (key == 's'){
-        
-        //  Save All View Ports positions
-        //
-        videoViewPort.saveSettings();
-        mapamoko01.saveSettings();
-        mapamoko02.saveSettings();
+        mapamoko.saveSettings();
     } else if (key == 'S'){
-        
-        //  Save Calibrations
-        //
-        mapamoko01.saveCalibration();
+        mapamoko.saveCalibration();
     }
 }
 //--------------------------------------------------------------
--- src/testApp.h
+++ src/testApp.h
@@ -5,23 +5,6 @@
 #include "ofxMapamok.h"
 #include "ofxSmartViewPort.h"
 
-//  Natxo,
-//          Aqui implemente el canvas mode al cual entras con 'c'
-//  pero q puedes editar con 'e'. Si quieres mover los view ports
-//  de los MapaMokos lo haces con 'E'
-//
-
-//  Luego con SPACE cambias el modo de setup. Fijate que ahora esta
-//  todo mejor explicado. Te va guiando como continuar
-//  Asi mismo puedes cambiar el modo de dibujo con TAB
-//
-
-//  Todo ahora est‡ m‡s sencillo y encapsulado.
-//  Quiz‡ demasiado. Av’same cualquier cosa.
-//
-
-//
-
 class testApp : public ofBaseApp{
 public:
     void setup();
@@ -32,18 +15,8 @@ public:
     void mousePressed(int x, int y, int button);
     void mouseReleased(int x, int y, int button);
     
-    ofxMapaMok      mapamoko01;
-    ofxMapaMok      mapamoko02;
-    
-    
-    ofVideoGrabber      video;
-    ofxSmartViewPort    videoViewPort;
+    ofxMapaMok      mapamoko;
     
     ofShader        shader;
     ofFbo           canvas;
-    
-    int             textWidth;
-    int             textHeight;
-    
-    bool            bCanvasMode;
 };
